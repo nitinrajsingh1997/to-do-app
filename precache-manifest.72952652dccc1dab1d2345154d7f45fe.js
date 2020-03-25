77aec24ae7eeb471b5ad46de6a8ff77ede56cbd0
Nitin Raj Singh <nitinrajsingh430@gmail.com>
2020-03-25 21:23:23 +0530

    Updates

---
+++ precache-manifest.72952652dccc1dab1d2345154d7f45fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "809a88e9a75ff0e842045a82d9150441",
+    "url": "/to-do-app/index.html"
+  },
+  {
+    "revision": "4a6a08792b2262d91469",
+    "url": "/to-do-app/static/css/main.f00f6a51.chunk.css"
+  },
+  {
+    "revision": "70d13f58a4a471c02542",
+    "url": "/to-do-app/static/js/2.349eca96.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app/static/js/2.349eca96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a6a08792b2262d91469",
+    "url": "/to-do-app/static/js/main.8eeb213b.chunk.js"
+  },
+  {
+    "revision": "7a2f6aeddf6bcb2b93cb",
+    "url": "/to-do-app/static/js/runtime-main.0705fcdf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
